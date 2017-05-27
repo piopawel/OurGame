01adef2c179735b63bfb8aa3fef5a1998a47c0e2
Piotr Pawelczyk <piopawelczyk@gmail.com>
2017-05-27 22:15:32 +0200

    Added DatabaseConnector Connection with Db is working - it is possible to take the names of players directly from Db

---
+++ Assets/Classes/DatabaseConnector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Data.SqliteClient;
+using System.Data;
+using System.IO;
+
+namespace Assets.Classes
+{
+    class DatabaseConnector
+    {
+        static string path = Directory.GetCurrentDirectory();
+        static string conn = "URI=file:" + path + "\\Assets\\Plugins\\Database.db";
+        public List<string> connect()
+        {
+            List<string> usernames = new List<string> {};
+            IDbConnection dbconn = (IDbConnection) new SqliteConnection(conn);
+            dbconn.Open();
+            IDbCommand dbcmd = dbconn.CreateCommand();
+            string sqlQuery = "SELECT username from Players ";
+            dbcmd.CommandText = sqlQuery;
+            IDataReader reader = dbcmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string username = reader.GetString(0);
+                usernames.Add(username);
+                
+            }
+            reader.Close();
+            reader = null;
+            dbcmd.Dispose();
+            dbcmd = null;
+            dbconn.Close();
+            dbconn = null;
+            //usernames.Add(conn);
+            return usernames;
+        }
+        
+    }
+
+}
