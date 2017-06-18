1bb041526fd9ef6795bff34fa6fdf4ac87dafebb
Mamelska <marta.mamelska@gmail.com>
2017-06-18 17:09:01 +0200

    game1 almost finished, added game 2

--- Assets/Scripts/Game1.cs
+++ Assets/Scripts/Game1.cs
@@ -2,55 +2,122 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Game1 : MonoBehaviour {
+public class Game1 : MonoBehaviour
+{
 
     public TextMesh questionDisplay1;
     public TextMesh questionDisplay2;
     public TextMesh answerDisplay;
-    private int variableA;
-    private int variableB;
-    private string sign;
-    private double answer;
-    List<string> signs = new List<string>(new string[] {"+", "-", "*", "/" });
-
-	// Use this for initialization
-	void Start () {
+    static int variableA;
+    static int variableB;
+    static string sign;
+    static float answer;
+    List<string> signs = new List<string>{ "/", "+", "-", "*" };
 
 
-        nextQuestion();
-		
-	}
-	
-    
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-    public void checkGame(int checkedSign) {
-
-        if (checkedSign == 1)
-        {
-
-            nextQuestion();
-            
-
-        }
-        else {
-
-            questionDisplay1.text = "error";    
-        
-        }
+        nextQuestion(10);
+        Debug.Log("START" + answer + " == " + variableA + " ? " + variableB);
     }
 
-    public void nextQuestion(){
-    
-    
-        variableA = UnityEngine.Random.Range(0, 10);
-        variableB = UnityEngine.Random.Range(0, 10);
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void checkGame(string checkedSign)
+    {
+
+
+        switch (checkedSign)
+        {
+            case "+":
+                Debug.Log(answer + " == " + variableA + " + " + variableB);
+                if (answer == variableA + variableB)
+                {
+                    Debug.Log(answer + " == " + variableA + " + " + variableB);
+                    nextQuestion(10);
+                };
+                break;
+            case "-":
+                Debug.Log(answer + " == " + variableA + " - " + variableB);
+                if (answer == variableA - variableB)
+                {
+                    Debug.Log(answer + " == " + variableA + " - " + variableB);
+                    nextQuestion(10);
+                };
+                break;
+            case "/":
+                Debug.Log(answer + " == " + variableA + " / " + variableB);
+                if (answer == variableA / variableB)
+                {
+                    Debug.Log(answer + " == " + variableA + " / " + variableB);
+                    nextQuestion(10);
+                };
+                break;
+            case "*":
+
+                Debug.Log(answer + " == " + variableA + " * " + variableB);
+                if (answer == variableA * variableB)
+                {
+                    Debug.Log(answer + " == " + variableA + " * " + variableB);
+                    nextQuestion(10);
+                };
+                break;
+
+            default:
+                Debug.Log("błąd ");
+                break;
+
+        }
+
+
+    }
+
+    public void nextQuestion(int level)
+    {
+
+        variableA = UnityEngine.Random.Range(0, level);
+        Debug.Log("variableA " + variableA);
         sign = signs[UnityEngine.Random.Range(0, 4)];
 
-        
+
+        switch (sign)
+        {
+            case "/":
+                variableB = UnityEngine.Random.Range(1, level);
+                Debug.Log("variableB " + variableB);
+                break;
+
+            default:
+                variableB = UnityEngine.Random.Range(0, level);
+                break;
+        }
+
+        switch (sign)
+        {
+            case "/":
+                while (variableA % variableB != 0)
+                {
+                    variableA = UnityEngine.Random.Range(0, level);
+                    Debug.Log("variableA " + variableA);
+                }
+
+                break;
+
+        }
+
+        nextQuestionDisplay();
+    }
+
+    public void nextQuestionDisplay()
+    {
+
         questionDisplay1.text = variableA.ToString();
         questionDisplay2.text = variableB.ToString();
 
@@ -71,12 +138,11 @@ public class Game1 : MonoBehaviour {
                 answer = variableA * variableB;
                 break;
 
-               
+
         }
 
         answerDisplay.text = answer.ToString();
-    
+
     }
-    
-    
+
 }
