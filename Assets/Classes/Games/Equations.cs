1a8b6a02bfc5389a0c5227b4808105b4ada887f3
Piotr Pawelczyk <piopawelczyk@gmail.com>
2017-06-18 17:52:21 +0200

    Added Time and Points to Game1

---
+++ Assets/Classes/Games/Equations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Classes.Games
+{
+    class Equations
+    {
+        public static string player = "PIO";
+        public static float points;
+        public static int gameMode;
+    }
+}
---
+++ Assets/Scripts/Game1/EqPointDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Classes.Games;
+
+public class EqPointDisplay : MonoBehaviour
+{
+    public GameObject pointLabel;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointLabel.GetComponentInChildren<TextMesh>().text = "Odp:" + Equations.points;
+    }
+}
